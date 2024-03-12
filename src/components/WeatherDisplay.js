15a73e8e23baa16779cc1837e3aedddcf5b0da72
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-03-12 11:18:28 +0000

    error solved

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-const WeatherDisplay=(props)=>{
-    const {temperature,conditions} = props.data;
-    return(<>
-    <p style={temperature>20?{color:'red':{color:'black'}}>Temperature: {temperature}</p>
-    <p>conditions:{conditions}</p>
-    </>)
-}
+const WeatherDisplay = (props) => {
+  const { temperature, conditions } = props.data;
 
+  return (
+    <>
+      <p style={{ color: temperature > 20 ? 'red' : 'black' }}>
+        Temperature: {temperature}
+      </p>
+      <p>Conditions: {conditions}</p>
+    </>
+  );
+};
 
 export default WeatherDisplay;
